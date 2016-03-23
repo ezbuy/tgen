b8fc3a70d099272b7a2303317ad72e4d248d43df
dtynn <dtynn@163.com>
2016-03-23 21:16:32 +0800

    去掉分号

--- example/golang/Const.thrift
+++ example/golang/Const.thrift
@@ -1,19 +1,19 @@
 namespace go constant
 
-const i16 ConstI16 = 16;
-const i32 ConstI32 = 32;
-const i64 ConstI64 = 64;
-const double ConstDouble = 128.128;
+const i16 ConstI16 = 16
+const i32 ConstI32 = 32
+const i64 ConstI64 = 64
+const double ConstDouble = 128.128
 
 const bool ConstBool = true
 const byte ConstByte = 65
 
-const binary ConstBinary = "binary";
-const string ConstString = "string";
+const binary ConstBinary = "binary"
+const string ConstString = "string"
 
-const list<string> ConstListString = ["a", "b", "c"];
-const list<i32> ConstListI32 = [4, 3, 2, 1];
+const list<string> ConstListString = ["a", "b", "c"]
+const list<i32> ConstListI32 = [4, 3, 2, 1]
 
-const map<string, string> ConstMapStringString = {"hello": "world", "Alpha": "Go"};
-const map<string, i32> ConstMapStringI32 = {"1": 2, "2": 4};
+const map<string, string> ConstMapStringString = {"hello": "world", "Alpha": "Go"}
+const map<string, i32> ConstMapStringI32 = {"1": 2, "2": 4}
 
