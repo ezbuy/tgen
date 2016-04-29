662bf6d096fe60dc0278db90b4321faf413c8ac2
hongjianzhu <zhu.life@gmail.com>
2016-04-29 10:22:57 +0800

    add enum support

--- langs/swift/swift_gen.go
+++ langs/swift/swift_gen.go
@@ -28,11 +28,12 @@ const (
 )
 
 const (
-	TPL_STRUCT  = "tgen/swift/struct"
-	TPL_SERVICE = "tgen/swift/serivce"
+	tplStruct  = "tgen/swift/struct"
+	tplEnum    = "tgen/swift/enum"
+	tplService = "tgen/swift/serivce"
 )
 
-var typemapping = map[string]string{
+var typeMapping = map[string]string{
 	langs.ThriftTypeI16:    SwiftTypeInt,
 	langs.ThriftTypeI32:    SwiftTypeInt,
 	langs.ThriftTypeI64:    SwiftTypeInt64,
@@ -49,13 +50,13 @@ type SwiftGen struct {
 type BaseSwift struct {
 	Filepath string
 	Thrift   *parser.Thrift
-	Thrifts  *map[string]*parser.Thrift // use pointer to pass variable, so it doesn't need to copy
+	Thrifts  map[string]*parser.Thrift
 }
 
 func (this *BaseSwift) PlainType(t *parser.Type) string {
 	n := t.Name
 
-	if t, ok := typemapping[n]; ok {
+	if t, ok := typeMapping[n]; ok {
 		return t
 	}
 
@@ -67,6 +68,45 @@ func (this *BaseSwift) PlainType(t *parser.Type) string {
 	default:
 		return this.AssembleCustomizedTypeName(t)
 	}
+}
+
+// IsEnum checks whether a type is enum.
+// it first checks in its own definition, than check from included files
+func (b *BaseSwift) IsEnum(t *parser.Type) bool {
+	if t == nil {
+		return false
+	}
+
+	names := strings.Split(t.Name, ".")
+
+	if len(names) == 1 {
+		for n := range b.Thrift.Enums {
+			if n == t.Name {
+				return true
+			}
+		}
+
+		return false
+	}
+
+	for path, thrift := range b.Thrifts {
+		if thrift == b.Thrift {
+			continue
+		}
+
+		filename := strings.TrimSuffix(filepath.Base(path), filepath.Ext(path))
+		if filename != names[0] {
+			continue
+		}
+
+		for n := range thrift.Enums {
+			if n == names[1] {
+				return true
+			}
+		}
+	}
+
+	return false
 }
 
 func (this *BaseSwift) GetInnerType(t *parser.Type) string {
@@ -111,9 +151,19 @@ func (this *BaseSwift) AssembleCustomizedTypeName(t *parser.Type) string {
 
 			return fmt.Sprintf("%s%s", ns, t.Name[1:])
 		}
+
+		for n := range this.Thrift.Enums {
+			if n != t.Name {
+				continue
+			}
+
+			ns, _ := this.Thrift.Namespaces["swift"]
+
+			return fmt.Sprintf("%s%s", ns, t.Name[1:])
+		}
 	}
 
-	for path, thrift := range *this.Thrifts {
+	for path, thrift := range this.Thrifts {
 		if thrift == this.Thrift {
 			continue
 		}
@@ -132,13 +182,26 @@ func (this *BaseSwift) AssembleCustomizedTypeName(t *parser.Type) string {
 
 			return fmt.Sprintf("%s%s", ns, names[1][1:])
 		}
+
+		for n := range thrift.Enums {
+			if n != names[1] {
+				continue
+			}
+
+			ns, _ := thrift.Namespaces["swift"]
+
+			return fmt.Sprintf("%s%s", ns, names[1][1:])
+		}
 	}
 
-	panic(fmt.Sprintf("thrift file '%s': namespace of customized type '%s' if not found\n", this.Filepath, t.Name))
+	log.Fatalf("thrift file '%s': namespace of customized type '%s' if not found\n", this.Filepath, t.Name)
+
+	return ""
 }
 
-func (this *BaseSwift) AssembleStructName(n string) string {
-	ns, _ := this.Thrift.Namespaces["swift"]
+// AssembleStructName returns object name of generated file name, assembled with namespace
+func (b *BaseSwift) AssembleStructName(n string) string {
+	ns, _ := b.Thrift.Namespaces["swift"]
 	return fmt.Sprintf("%s%s", ns, n[1:])
 }
 
@@ -171,28 +234,31 @@ func (this *BaseSwift) ParamsJoinedByComma(args []*parser.Field) string {
 	return buf.String()
 }
 
-func (this *BaseSwift) AssignToDict(f *parser.Field) string {
+func (b *BaseSwift) AssignToDict(f *parser.Field) string {
 	if f.Type.Name == "list" {
-		innertype := this.GetInnerType(f.Type)
-		if this.IsBasicType(innertype) {
+		innertype := b.GetInnerType(f.Type)
+		if b.IsBasicType(innertype) {
 			if innertype == SwiftTypeInt64 {
-				return fmt.Sprintf("%s?.map { value in NSNumber(longLong: value) }", this.FilterPropertory(f.Name))
+				return fmt.Sprintf("%s?.map { value in NSNumber(longLong: value) }", b.FilterPropertory(f.Name))
 			}
-			return fmt.Sprintf("%s", this.FilterPropertory(f.Name))
-		} else {
-			return fmt.Sprintf("%s?.toJSON()", this.FilterPropertory(f.Name))
+			return fmt.Sprintf("%s", b.FilterPropertory(f.Name))
 		}
+
+		return fmt.Sprintf("%s?.toJSON()", b.FilterPropertory(f.Name))
 	}
 
 	switch f.Type.Name {
 	case langs.ThriftTypeI16, langs.ThriftTypeI32, langs.ThriftTypeByte, langs.ThriftTypeString,
 		langs.ThriftTypeBool, langs.ThriftTypeDouble,
 		langs.ThriftTypeMap:
-		return this.FilterPropertory(f.Name)
+		return b.FilterPropertory(f.Name)
 	case langs.ThriftTypeI64:
-		return fmt.Sprintf("NSNumber(longLong: %s)", this.FilterPropertory(f.Name))
+		return fmt.Sprintf("NSNumber(longLong: %s)", b.FilterPropertory(f.Name))
 	default:
-		return fmt.Sprintf("%s?.toJSON()", this.FilterPropertory(f.Name))
+		if b.IsEnum(f.Type) {
+			return fmt.Sprintf("%s?.rawValue", b.FilterPropertory(f.Name))
+		}
+		return fmt.Sprintf("%s?.toJSON()", b.FilterPropertory(f.Name))
 	}
 }
 
@@ -226,8 +292,14 @@ func (this *BaseSwift) Typecast(t *parser.Type, flag bool) string {
 		}
 		return pt
 	default:
+
 		return fmt.Sprintf("%s?", pt)
 	}
+}
+
+type swiftEnum struct {
+	*BaseSwift
+	*parser.Enum
 }
 
 type swiftStruct struct {
@@ -252,12 +324,14 @@ func generateWithModel(gen *SwiftGen, m string, output string, parsedThrift map[
 	}
 
 	// init templates
-	structpl := initemplate(TPL_STRUCT, "tmpl/swift/struct.goswift")
+	structpl := initemplate(tplStruct, "tmpl/swift/struct.goswift")
+	enumtpl := initemplate(tplEnum, "tmpl/swift/enum.goswift")
+
 	var servicetpl *template.Template
 	if m == global.MODE_REST {
-		servicetpl = initemplate(TPL_SERVICE, "tmpl/swift/rest_service.goswift")
+		servicetpl = initemplate(tplService, "tmpl/swift/rest_service.goswift")
 	} else if m == global.MODE_JSONRPC {
-		servicetpl = initemplate(TPL_SERVICE, "tmpl/swift/jsonrpc_service.goswift")
+		servicetpl = initemplate(tplService, "tmpl/swift/jsonrpc_service.goswift")
 	}
 
 	wg := sync.WaitGroup{}
@@ -268,21 +342,40 @@ func generateWithModel(gen *SwiftGen, m string, output string, parsedThrift map[
 			continue // ignore
 		}
 
+		// enum
+		wg.Add(1)
+
+		go func(t *parser.Thrift, f string) {
+			defer wg.Done()
+
+			for _, e := range t.Enums {
+				baseSwift := &BaseSwift{Filepath: f, Thrift: t, Thrifts: parsedThrift}
+
+				name := fmt.Sprintf("%s.swift", baseSwift.AssembleStructName(e.Name))
+				path := filepath.Join(output, name)
+
+				data := &swiftEnum{BaseSwift: baseSwift, Enum: e}
+
+				if err := outputfile(path, enumtpl, tplEnum, data); err != nil {
+					panic(fmt.Errorf("failed to write file %s. error: %v\n", path, err))
+				}
+			}
+		}(t, f)
+
 		wg.Add(1)
 
 		go func(t *parser.Thrift, f string) {
 			defer wg.Done()
 
 			for _, s := range t.Structs {
-				baseSwift := &BaseSwift{Filepath: f, Thrift: t, Thrifts: &parsedThrift}
+				b := &BaseSwift{Filepath: f, Thrift: t, Thrifts: parsedThrift}
 
-				name := fmt.Sprintf("%s.swift", baseSwift.AssembleStructName(s.Name))
-
+				name := fmt.Sprintf("%s.swift", b.AssembleStructName(s.Name))
 				path := filepath.Join(output, name)
 
-				data := &swiftStruct{BaseSwift: baseSwift, Struct: s}
+				data := &swiftStruct{BaseSwift: b, Struct: s}
 
-				if err := outputfile(path, structpl, TPL_STRUCT, data); err != nil {
+				if err := outputfile(path, structpl, tplStruct, data); err != nil {
 					panic(fmt.Errorf("failed to write file %s. error: %v\n", path, err))
 				}
 			}
@@ -296,12 +389,11 @@ func generateWithModel(gen *SwiftGen, m string, output string, parsedThrift map[
 			for _, s := range t.Services {
 				// filename is the service name plus 'Service'
 				name := s.Name + "Service.swift"
-
 				path := filepath.Join(output, name)
 
-				data := &swiftService{BaseSwift: &BaseSwift{Filepath: f, Thrift: t, Thrifts: &parsedThrift}, Service: s}
+				data := &swiftService{BaseSwift: &BaseSwift{Filepath: f, Thrift: t, Thrifts: parsedThrift}, Service: s}
 
-				if err := outputfile(path, servicetpl, TPL_SERVICE, data); err != nil {
+				if err := outputfile(path, servicetpl, tplService, data); err != nil {
 					panic(fmt.Errorf("failed to write file %s. error: %v\n", path, err))
 				}
 			}
@@ -316,11 +408,12 @@ func generateAll(gen *SwiftGen, output string, parsedThrift map[string]*parser.T
 	generateWithModel(gen, global.MODE_JSONRPC, filepath.Join(output, global.MODE_JSONRPC), parsedThrift)
 }
 
-func (this *SwiftGen) Generate(output string, parsedThrift map[string]*parser.Thrift) {
+// Generate is the entry of the logic
+func (g *SwiftGen) Generate(output string, parsedThrift map[string]*parser.Thrift) {
 	if global.Mode != "" {
-		generateWithModel(this, global.Mode, output, parsedThrift)
+		generateWithModel(g, global.Mode, output, parsedThrift)
 	} else {
-		generateAll(this, output, parsedThrift)
+		generateAll(g, output, parsedThrift)
 	}
 }
 
